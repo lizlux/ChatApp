c91ceb759542d1feec6b76ddecf289ecc7611bc3
lizlux <liz@lizlux.net>
2013-12-12 14:38:46 -0800

    got the tap game to work

--- public/javascript/leap_gestures.js
+++ public/javascript/leap_gestures.js
@@ -3,31 +3,19 @@ $( function() {
 	var controller = new Leap.Controller({
 			enableGestures: true
 		}),
+		$window = $( window ),
 		$body = $( 'body' ),
 		$wrapper = $( '#wrapper' ),
 		$dot = $( '<span>' )
 			.addClass( 'dot' ),
-		$finger = $( '<span>' )
-			.addClass( 'finger' ),
-		xOffset = $wrapper.width() / 2,
-		yOffset = $wrapper.height(),
-		scale = 2;
-
-	// Add divider to page
-	$( '<div>' )
-		.attr( 'id', 'divider' )
-		.appendTo( $body );
-
-	controller.on( 'gesture', function ( gesture ){
-		if( gesture.type === 'keyTap' || gesture.type === 'screenTap' ){
-			console.log( gesture );
-			addDot( gesture );
-			selectPointed( gesture );
-		}
-	});
-
+		$finger = $( '<div>' )
+			.attr( 'id', 'finger' )
+			.appendTo( $wrapper ),
+		width = $window.width(),
+		height = $window.height();
 
 	function leapToScene( frame, leapPos ){
+
 		var iBox = frame.interactionBox,
 			top = iBox.center[1] + iBox.size[1]/ 2,
 			left = iBox.center[0] - iBox.size[0]/ 2,
@@ -43,9 +31,57 @@ $( function() {
 		return [ x, -y ];
 	}
 
+	// Tells the controller what to do every time it sees a frame
+	controller.on( 'frame' , function( frame ){
+		var showFinger = false,
+			// get the first hand we see
+			hand = frame.hands[0];
+
+		if( hand ) {
+			// grab the first finger we see
+			var finger = hand.fingers[0];
+
+			if( finger ) {
+				showFinger = true;
+				// and get its position on the screen
+				var fingerPos = leapToScene( frame, finger.tipPosition );
+
+				$finger.css( 'left', fingerPos[0] + 'px' )
+					.css( 'top', fingerPos[1] + 'px' );
+
+				//console.log( frame.gestures );
+
+				if( frame.gestures.length && ( frame.gestures[0].type == 'screenTap' || frame.gestures[0].type == 'keyTap' ) ) {
+					var $elem = $( document.elementFromPoint( fingerPos[0], fingerPos[1] ) );
+
+					if( $elem.is( '.box' ) ) {
+					    $elem.addClass( 'selected' );
+					}
+				}
+
+			}
+		}
+
+		if( showFinger ) {
+			$finger.show();
+		} else {
+			$finger.hide();
+		}
+	});
 
 
+	// Add divider to page
+	$( '<div>' )
+		.attr( 'id', 'divider' )
+		.appendTo( $body );
 
+	controller.on( 'gesture', function ( gesture ){
+		if( gesture.type === 'screenTap' ){
+			//console.log( gesture );
+			//addDot( gesture );
+			//selectPointed( gesture );
+		}
+	});
 
 	function selectPointed( gesture ) {
 		var left = getLeft( gesture ),
@@ -76,4 +112,5 @@ $( function() {
 	}
 
 	controller.connect();
+	console.log( controller );
 } );
