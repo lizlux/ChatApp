9ad3001d8fc330dfcf21e06629a4f16a75857490
lizlux <liz@lizlux.net>
2013-12-13 10:07:05 -0800

    selecting and scrolling with leap

--- public/javascript/leap_gestures.js
+++ public/javascript/leap_gestures.js
@@ -6,7 +6,7 @@ $( function() {
 		$window = $( window ),
 		$body = $( 'body' ),
 		$wrapper = $( '#wrapper' ),
-		$dot = $( '<span>' )
+		$dot = $( '<div>' )
 			.addClass( 'dot' ),
 		$finger = $( '<div>' )
 			.attr( 'id', 'finger' )
@@ -49,18 +49,57 @@ $( function() {
 				$finger.css( 'left', fingerPos[0] + 'px' )
 					.css( 'top', fingerPos[1] + 'px' );
 
-				//console.log( frame.gestures );
-
 				if( frame.gestures.length && ( frame.gestures[0].type == 'screenTap' || frame.gestures[0].type == 'keyTap' ) ) {
+					// TODO: this is no longer working - it's getting an element below it
 					var $elem = $( document.elementFromPoint( fingerPos[0], fingerPos[1] ) );
+
+					console.log( $elem );
 
 					if( $elem.is( '.box' ) ) {
 					    $elem.addClass( 'selected' );
+					} else {
+						addDot( $dot.clone(), fingerPos[0], fingerPos[1] );
 					}
 				}
-
 			}
 		}
+
+		// scroll up and down
+		if (frame.gestures.length) {
+			console.log( 'frame' );
+			var swipeDirection;
+			for (var i = 0; i < frame.gestures.length; i++) {
+				var gesture = frame.gestures[i];
+
+				if (gesture.type == "swipe") {
+					//Classify swipe as either horizontal or vertical
+					var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+					//Classify as right-left or up-down
+					if(isHorizontal){
+						if(gesture.direction[0] > 0){
+							swipeDirection = "right";
+						} else {
+							swipeDirection = "left";
+						}
+					} else { //vertical
+						if(gesture.direction[1] > 0){
+							swipeDirection = "up";
+						} else {
+							swipeDirection = "down";
+						}
+					}
+				}
+			}
+			if( swipeDirection == 'up' ) {
+				console.log( 'scroll up' );
+				document.body.scrollTop = document.body.scrollTop - 10;
+			} else if( swipeDirection == 'down' ) {
+				console.log( 'scroll down' );
+				document.body.scrollTop = document.body.scrollTop + 10;
+			}
+
+		}
+
 
 		if( showFinger ) {
 			$finger.show();
@@ -69,48 +108,52 @@ $( function() {
 		}
 	});
 
+	function pageScroll( direction ) {
+		var px = 20;
 
-	// Add divider to page
-	$( '<div>' )
-		.attr( 'id', 'divider' )
-		.appendTo( $body );
+		setInterval( function() {
+			document.body.scrollTop = document.body.scrollTop - px--;
+		}, 200 );
 
-	controller.on( 'gesture', function ( gesture ){
-		if( gesture.type === 'screenTap' ){
-			//console.log( gesture );
-			//addDot( gesture );
-			//selectPointed( gesture );
-		}
-	});
 
-	function selectPointed( gesture ) {
-		var left = getLeft( gesture ),
-			top =  getTop( gesture ),
-			elem = document.elementFromPoint( left, top );
-
-		$( elem ).addClass( 'selected' );
 	}
 
-	function addDot( gesture ) {
-		var id = 'dot_' + gesture.id,
-			left = getLeft( gesture ),
-			top =  getTop( gesture );
 
-		$dot.clone()
-			.attr( 'id', id )
+	function addDot( $dot, left, top ) {
+		console.log( 'adding dot' );
+		$dot
 			.css( 'left', left )
 			.css( 'top', top )
 			.appendTo( $wrapper );
+
+		setTimeout( function() {
+			$dot.fadeOut( 'fast', function () {
+				$dot.remove();
+			} );
+		}, 1000 );
 	}
 
-	function getLeft( gesture ) {
-		return ( gesture.position[0] * scale ) + xOffset;
-	}
+	/*controller.on( 'connect', function () {
+		console.log( 'connected' );
+		var e={};
+		var t={};
+		var n=document.body;
+		Leap.loop(function(t){
+			var r={};
+			var i={};
+			for(var s=0, o=t.pointables.length; s!=o; s++){
+				var u=t.pointables[s];
+				var a=e[u.id];
+				var f=n.scrollTop;
+				if(document.hasFocus()){
+					if(u.tipPosition[1]-325>0){n.scrollTop=f-=150}
+					if(u.tipPosition[1]-125>0){n.scrollTop=f-=5}
+					if(u.tipPosition[1]-90<0){n.scrollTop=f+=5}
+				}
+			}
+		})
+	} );*/
 
-	function getTop( gesture ) {
-		return yOffset - ( gesture.position[1] * scale );
-	}
 
 	controller.connect();
-	console.log( controller );
 } );
